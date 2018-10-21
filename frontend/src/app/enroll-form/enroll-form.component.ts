2c30c2ffa63ef13c5708976940e57279d61c04e1
= <=>
2018-10-21 15:09:18 -0500

    changeing logic for user enrolment and fixing bug

--- frontend/src/app/enroll-form/enroll-form.component.ts
+++ frontend/src/app/enroll-form/enroll-form.component.ts
@@ -1,4 +1,8 @@
+import { select, NgRedux } from '@angular-redux/store';
 import { Component, OnInit } from '@angular/core';
+import { IStudent } from '../redux/student';
+import { StudentService } from '../services/student.service';
+import { CREATE_STUDENT, UPDATE_STUDENT} from '../redux/actions'
 
 @Component({
   selector: 'app-enroll-form',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnrollFormComponent implements OnInit {
 
-  constructor() { }
+ 
+  @select() students;
+
+  model: IStudent = {
+    status: "new",
+    enrollmentData: '',
+    enrollmentForm: [],
+    examQuestions: [],
+    monitoring: [],
+    snapshots: [],
+    grading: [],
+    resultsSent: false
+  }
+
+  constructor(private studentService: StudentService, private ngRedux: NgRedux<IStudent>) {}
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    const action = this.ngRedux.dispatch({type: CREATE_STUDENT, student: this.model}); 
+    console.log('New Student');
+    console.log(action.student);
+    this.studentService.createStudent(action.student);
   }
 
 }
--- frontend/src/app/new-students/new-students.component.ts
+++ frontend/src/app/new-students/new-students.component.ts
@@ -1,9 +1,4 @@
-import { select, NgRedux } from '@angular-redux/store';
 import { Component, OnInit } from '@angular/core';
-import { IStudent } from '../redux/student';
-import { StudentService } from '../services/student.service';
-import { CREATE_STUDENT, UPDATE_STUDENT} from '../redux/actions'
-
 
 @Component({
   selector: 'app-new-students',
@@ -12,26 +7,9 @@ import { CREATE_STUDENT, UPDATE_STUDENT} from '../redux/actions'
 })
 export class NewStudentsComponent implements OnInit {
 
-  @select() students;
-
-  model: IStudent = {
-    status: "new",
-    enrollmentData: '',
-    enrollmentForm: [],
-    examQuestions: [],
-    monitoring: [],
-    snapshots: [],
-    grading: [],
-    resultsSent: false
-  }
-
-  constructor(private studentService: StudentService, private ngRedux: NgRedux<IStudent>) {}
+  constructor() { }
 
   ngOnInit() {
-    const action = this.ngRedux.dispatch({type: CREATE_STUDENT, student: this.model}); 
-    console.log('New Student');
-    console.log(action.student);
-    this.studentService.createStudent(action.student)
-    }
+  }
 
 }
