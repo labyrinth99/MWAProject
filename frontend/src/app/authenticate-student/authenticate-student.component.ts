000ed27ef0a68402c6812d56f8cb921f815248e0
labyrinth81 <41203957+labyrinth81@users.noreply.github.com>
2018-10-23 19:35:56 -0500

    minor update

--- frontend/src/app/authenticate-student/authenticate-student.component.ts
+++ frontend/src/app/authenticate-student/authenticate-student.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../services/student.service';
+// import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-authenticate-student',
@@ -8,24 +10,39 @@ import { StudentService } from '../services/student.service';
 })
 export class AuthenticateStudentComponent implements OnInit {
 
-  public email;
+  public email:string;
+
+  user: Observable<any>;
+  emailSent = false;
+
+  errorMessage: string;
 
   constructor(private studentService: StudentService) { }
 
   ngOnInit() {
   }
   onSubmit(){
-
+    if(!this.email)
+    alert('Please Enter Your Email First');
+    else{
     this.studentService.getStudentByEmail(this.email)
     .subscribe( data => {
      console.log(data);
      if(data){
-     alert(this.email);
+    //  alert(this.email);
+
+
+
+
+
+    // end of autheni
      }
      else
      alert("This Email is not registered");
     },(error)=>console.log(error));
 
     }
+
+  }
 
 }
