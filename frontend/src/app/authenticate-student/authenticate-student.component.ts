4a9fe5bc62bf119ce2a06e0e32c5eadef5c2f833
labyrinth81 <41203957+labyrinth81@users.noreply.github.com>
2018-10-23 19:51:57 -0500

    npm install plz on frontend

--- frontend/src/app/authenticate-student/authenticate-student.component.ts
+++ frontend/src/app/authenticate-student/authenticate-student.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../services/student.service';
-// import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs';
 
 @Component({
